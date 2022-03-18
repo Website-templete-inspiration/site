fc568284f23d318510e829af8a6d967869914949
Daniele Barbaro <barbaro.daniele@gmail.com>
2022-03-18 22:04:32 +0100

    GTM + Data review

--- _data/user-data.ts
+++ _data/user-data.ts
@@ -1,17 +1,9 @@
 export const me = {
     name: "Daniele Barbaro",
     who: "Full-Stack Developer",
-    avatarUrl: "/avatar.png",
+    avatarUrl: "/daniele-barbaro.png",
     email: "barbarodaniele@gmail.com",
     phone: "0039 331 99 58 550",
     address: "Turin, Italy.",
-    about: {
-        title:
-            "Lorem ipsum",
-        description: [
-            `Lorem ipsum`,
-            `Lorem ipsum`,
-            `Lorem ipsum`,
-        ],
-    },
+    gtm: 'GTM-KXJ85JL'
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,21 @@
-import React from 'react';
-import { AppProps } from 'next/app';
-import { DefaultSeo } from 'next-seo';
-
+import React, {useEffect} from 'react';
+import {AppProps} from 'next/app';
+import {DefaultSeo} from 'next-seo';
 import '../styles/globals.css';
 
-import { OpenGraph } from '../_data/social-media';
+import {OpenGraph} from '../_data/social-media';
+import {ThemeProvider} from "next-themes";
+import TagManager from 'react-gtm-module';
+import {me} from "../_data/user-data";
 
-export default function App({ Component, pageProps }: AppProps) {
-
-  return (
-    <>
-      <DefaultSeo {...OpenGraph} />
-      <Component {...pageProps} />
-    </>
-  );
+export default function App({Component, pageProps}: AppProps) {
+    useEffect(() => {
+        TagManager.initialize({ gtmId: me.gtm });
+    }, []);
+    return (
+        <ThemeProvider defaultTheme="light" attribute="class">
+            <DefaultSeo {...OpenGraph} />
+            <Component {...pageProps} />
+        </ThemeProvider>
+    );
 }
