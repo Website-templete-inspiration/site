4d60e9e39d9aa7900bd7d6d6cf78b14a073e1d14
Daniele Barbaro <barbaro.daniele@gmail.com>
2022-03-19 09:19:29 +0100

    Just clean and fix react/no-unescaped-entities

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -12,16 +12,15 @@ export default function Hero() {
             <div className="flex flex-col lg:flex-row items-center my-2">
                 <div className="w-full md:w-2/3 text-center lg:text-left p-4">
                     <h2 className="font-normal text-xl lg:text-2xl max-w-3xl">
-                        Hello Dude, I'm <span className="font-bold">Daniele</span>, the one man show behind the
+                        Hello Dude, I&apos;m <span className="font-bold">Daniele</span>, the one man show behind the
                         curtain <Highlight color={'#21759B'} type={'box'}> <span className="font-bold">dB
                         Development</span></Highlight>.
                     </h2>
 
                     <h2 className="font-normal text-xl lg:text-2xl max-w-3xl pt-6">
-                        I <Highlight color={'#8b9b21'} type={'box'}>code</Highlight>, I <Highlight color={'#6a9b21'}
-                                                                                                   type={'box'}>write
-                        articles</Highlight> on <a href={SocialMedia.Medium}>Medium</a> and I <Highlight
-                        color={'#249a20'} type={'box'}>teach how to write beautiful code</Highlight>.
+                        I <Highlight color={'#8b9b21'} type={'box'}>code</Highlight>, I <Highlight
+                        color={'#249a20'} type={'box'}>teach how to write beautiful code</Highlight> and
+                        I <Highlight color={'#6a9b21'} type={'box'}>write articles</Highlight> on <a href={SocialMedia.Medium}>Medium</a> and.
                     </h2>
                 </div>
                 <div className="w-full md:w-1/3 lg:block items-end p-4   ">
@@ -53,7 +52,7 @@ export default function Hero() {
                     </h2>
                     <div className="hidden sm:block pt-4 ">
                         <p className="font-mono font-extralight text-sm">
-                            <BsArrowLeft className="inline-block animate-ping mr-1 "/> That's my 10 years
+                            <BsArrowLeft className="inline-block animate-ping mr-1 "/> That&apos;s my 10 years
                             company <GiPartyPopper className="inline-block animate-ping ml-0 "/>
                         </p>
                     </div>
